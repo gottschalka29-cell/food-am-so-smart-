dfb96d9eb7949bd86c848cdbd7d3dc2a5d2b5fd6
gottschalka29-cell <gottschalka29@bcdschool.org>
2025-09-16 21:00:31 +0000

    d

---
+++ e.py
@@ -0,0 +1,36 @@
+import math
+import random
+from abc import ABC, abstractmethod
+
+# Non_chicken = [ "Burger", "Hot dog", "Grilled cheese"]
+# Chicken = [ "Chicken nuggets", "Chicken sandwhich"]
+# Sauce = ["Ketchup", "Sweet n' Sour", "Mustard", "Mayonaise", "Spicy"]
+# sides = [ "Fruit cup", "Fries", "Brussel sprouts", "Salad", "Tomato soup"] 
+# secret_menu = {"Cursed Main":}
+# curse_main = ["'Mystery Meat' sandwhich", "'worker' sandwhich", "you know what I want.", "your shift is over."]
+# curse_side =["rocks","potentially lethal rocks", "'fries' dipped in 'red sauce'", "broccoli "] ]
+
+class Stage(ABC):
+    def __init__(self,customers):
+        self.customers =customers 
+        pass 
+    
+    
+
+    def order(self):
+        for i in range(self.customers): 
+            
+            print("A customer walks up and starts to order.")
+
+
+
+
+def game_start():
+
+    print("You wake up, you realize you start your new job today. Time to get driving.You arrive at the restauraunt, walking in. The manager greets you. Hi! Welcome to your new job at '!@#&*$%#@*'")
+    name = input("What's your name? ")
+    print("Welcome " + name + " to your new job. You can get started over there.")
+    print("You walk over to the cash register, a manual sitting next to it") 
+    input("the person operating this game will tell you how it works or there will be some sort of guide in person, type anything to continue; ")
+   #stage one begins 
+game_start()
